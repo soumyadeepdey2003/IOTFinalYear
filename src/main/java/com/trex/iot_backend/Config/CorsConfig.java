ee9198130b9ac7c9bac60b88be90bb38691b8600
soumyadeep dey <105117395+soumyadeepdey2003@users.noreply.github.com>
2025-06-19 20:42:14 +0530

    Update CorsConfig.java

--- src/main/java/com/trex/iot_backend/Config/CorsConfig.java
+++ src/main/java/com/trex/iot_backend/Config/CorsConfig.java
@@ -1,7 +1,4 @@
 package com.trex.iot_backend.Config;
-
-
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -10,13 +7,15 @@ import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class CorsConfig {
-
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
+        
+        config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedOrigin("http://129.154.237.212:3000");
+        
         config.addAllowedHeader("*");
         config.addAllowedMethod("OPTIONS");
         config.addAllowedMethod("GET");
